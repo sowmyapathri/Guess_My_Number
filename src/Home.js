15c77874100f0307d5e0a51c569bdd51de54b4aa
sowmyapathri <sowmyapathri@gmail.com>
2024-12-04 19:20:20 +0530

    modifications

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,5 @@
 import React, { useState } from 'react'
 import "./index.scss";
-//declare a number, score and high score
-// if given no is > then display no is gretaer or < declared no then the it fails and the score gets decreased by each attempt
-// if given no is equal to declared no then score should be consistent and the score should be compared
-// to high score if score is greater  than high score, then high score should be equal to score
-
 
 const Home = () => {
 
@@ -12,19 +7,17 @@ const Home = () => {
   const [highScore,setHighScore]=useState(10);
   const [status,setStatus]=useState("Start guessing...🤔");
   const [providedInput,setProvidedInput]=useState();
-  const [bgColor,setbgColor]=useState("normal");
-  let number = 15;
-  
+  const [guess,setGuess]=useState(Math.trunc(Math.random()*20));
+ 
   const checkNumber=()=>{
-   if(providedInput > number){
+   if(providedInput > guess){
       setStatus("Too High!😕");
       setScore(score-1);
-    }else if(providedInput < number){
+    }else if(providedInput < guess){
       setStatus("Too Low!😕");
       setScore(score-1);
-    }else if(providedInput == number){
+    }else if(providedInput == guess){
       setStatus("🎉Correct Answer!!😁");
-      setbgColor("success")
       if(score > highScore){
         setHighScore(score)
       }
@@ -36,7 +29,6 @@ const Home = () => {
 
 
   const handleChange=(e)=>{
-    // console.log(e.target.value);
     if(e.target.value > 20){
       setStatus("Please select in between 1 to 20😐")
     }else{
@@ -47,11 +39,11 @@ const Home = () => {
   
 const handleReset=()=>{
   setScore(20);
-  setbgColor("normal");
+  setGuess(Math.trunc((Math.random()*10)+10));
   setStatus("Start guessing...🤔")
 }
   return (
-    <div className={bgColor}>
+    <div>
         <h1>Guess My Number!</h1>
         <p style={{fontSize: "80%"}}>Number should be between 1 to 20</p>
         {score == 0 ? <div><p>"Sorry! 😓 You Lost.Try again" </p>
